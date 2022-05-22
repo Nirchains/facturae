132021bc03a5281a063a3570b5312967929e6d9e
nirchains <nirchains@gmail.com>
2022-05-22 18:41:56 +0000

    Custom button generar facturae

--- facturae/custom_script/sales_invoice.js
+++ facturae/custom_script/sales_invoice.js
@@ -6,7 +6,16 @@ cur_frm.add_fetch('customer_address', 'organo_proponente', 'organo_proponente');
 
 frappe.ui.form.on('Sales Invoice', {
 	refresh: function(frm) {
+		if (frm.doc.metodo_envio == "FACE") {
+			frm.add_custom_button(__("Generar FACTURAe"), function() {
+				var host = window.location.protocol + "//" + window.location.host;
+				var url = host + "/facturae/" + frm.doc.name;
+				window.open(url);
+			});	
+		}
+
 		frm.set_value("tc_name", "IVA 0");
 		frm.refresh_fields();
 	}
+	
 });
